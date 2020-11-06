2ba39cb65f94de57ea83bf0a7cf60135424fc872
Mumm <joshua.mumm@dfg.alaska.local>
2020-11-06 10:13:18 -0900

    fix several codes 149-156, restructure

---
+++ code/join.r
@@ -0,0 +1,10 @@
+library (tidyverse)
+
+read.csv("./data/Y_inventory_working.csv") %>% select(code, Path, Type) ->inv
+read.csv("./data/ALL_BrokenSrc20200803_1950_working.csv") -> brk
+
+brk %>% drop_na (code) %>% left_join (inv) %>%
+  transmute ( UniqID,dataType,newType = replace_na(Type, "_review"),brokenPath,newPath = Path) -> out
+
+out %>% write.csv('./out/roseta_201106.csv', row.names = FALSE)  
+
