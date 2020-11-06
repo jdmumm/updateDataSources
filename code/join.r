519784fa3dce4072f1695a1efc3a3521a4c7e559
Mumm <joshua.mumm@dfg.alaska.local>
2020-11-06 10:28:28 -0900

    minor

--- code/join.r
+++ code/join.r
@@ -6,5 +6,5 @@ read.csv("./data/ALL_BrokenSrc20200803_1950_working.csv") -> brk
 brk %>% drop_na (code) %>% left_join (inv) %>%
   transmute ( UniqID,dataType,newType = replace_na(Type, "_review"),brokenPath,newPath = Path) -> out
 
-out %>% write.csv('./out/roseta_201106.csv', row.names = FALSE)  
+out %>% write.csv('./out/rosetta_201106.csv', row.names = FALSE)  
 
